df40aeb962b5d4f17b7ac990a55a51b2d319f639
Luis Da Silva <luisdanield@gmail.com>
2019-02-28 01:37:06 +0000

    Alpha function changing. Better verbose in tune_fit_model

--- sklearnHelpers.py
+++ sklearnHelpers.py
@@ -34,15 +34,18 @@ def best_subset_selection(X, y, model, cv=5, scoring=None, verbose=1):
         init_time = dt.now()
         print("Initializing best subset selection process...")
 
-    best_score = 0
-    best_subset = []
+    all_first = True
 
     for i in range(1, n_features + 1):
         comb_iterator = combinations(all_features, i)
         for comb in comb_iterator:
             subset = list(comb)
             score = np.mean(cross_val_score(model, X[subset], y, cv=cv, scoring=scoring))
-            if score > best_score:
+            if all_first:
+                all_first = False
+                best_score = score
+                best_subset = subset
+            elif score > best_score:
                 best_score = score
                 best_subset = subset
 
@@ -68,18 +71,26 @@ def fw_cv_selection(X, y, model, cv=5, verbose=True, stopping=None, scoring=None
     
     all_features = list(X.columns)
     selected_features = []
-    best_score = 0
+    all_first = True
     no_update = 0
     
     for r in range(len(all_features)):
         rtime = dt.now()
         untested_features = [f for f in all_features if f not in selected_features]
-        best_score_this_round = 0
+        first = True
 
         for f1 in untested_features:
             features = selected_features + [f1]
             score = np.mean(cross_val_score(model, X[features], y, cv=cv, scoring=scoring))
-            if score > best_score_this_round:
+            if first:
+                first = False
+                best_score_this_round = score
+                best_feature_this_round = f1
+                if all_first:
+                    all_first = False
+                    best_score = score
+                    best_features = features
+            elif score > best_score_this_round:
                 best_score_this_round = score
                 best_feature_this_round = f1
                 if score > best_score:
@@ -149,7 +160,7 @@ def tune_fit_model(X, y, Model, param_grid=None, best_subset = False, forward_se
     else:
         model = Model
         params = model.get_params()
-    
+
     # Finding best subset
     
     if best_subset:
@@ -160,12 +171,13 @@ def tune_fit_model(X, y, Model, param_grid=None, best_subset = False, forward_se
     else:
         best_score = np.nan
         best_subset = list(X.columns)
-    
+
     # Tuning parameters
     if param_grid is not None:
         tune_model = GridSearchCV(model, param_grid, scoring=scoring, cv=cv)
         tune_model.fit(X[best_subset], y)
         params = tune_model.best_params_
+        model.set_params(**params)
 
     name = type(model).__name__
     results = {'name': name,
@@ -182,7 +194,7 @@ def tune_fit_model(X, y, Model, param_grid=None, best_subset = False, forward_se
         if params:
             print("Parameters: {}".format(params))
         print("-"*20)
-    
+
     model.fit(X[best_subset], y)
     results['model'] = model
     
@@ -196,18 +208,20 @@ def tune_fit_model(X, y, Model, param_grid=None, best_subset = False, forward_se
     return results
 
 
-def check_alpha_selection(algorithm, X, y, cv, best_score,
-                          best_alpha, multiplier,
-                          precision, direction = 1, change=10):
+def check_alpha_selection(algorithm, X, y, param, params, best_score,
+                          best_param, precision=0.0001, multiplier=1000000,
+                          cv=5, direction=1, change=10):
     """
     Test alpha value and look for a better one.
 
     :param algorithm: Un-called scikit-learn model that allows alpha
     :param X: X data frame
     :param y: target variable
+    :param param: String name of the param to fit.
+    :param params: Dictionary of params for model.
     :param cv: int. Number of folds for cross-validation.
     :param best_score: current best score
-    :param best_alpha: current best alpha
+    :param best_param: current best value for parameter.
     :param multiplier: searching strength
     :param precision: decimal point precision
     :param direction: 1 or -1
@@ -218,38 +232,39 @@ def check_alpha_selection(algorithm, X, y, cv, best_score,
     """
     if multiplier < 1:
         # Stop condition
-        return best_score, best_alpha, direction
+        return best_score, best_param, direction
     
     # Get new score (+)
-    new_alpha = best_alpha + precision * multiplier * direction
-    model = algorithm(alpha=new_alpha)
+    new_param = best_param + precision * multiplier * direction
+    params[param] = new_param
+    model = algorithm(**params)
     new_score = np.mean(cross_val_score(model, X, y, cv=cv))
     
     if new_score > best_score:
         # Better alpha found
         best_score = new_score
-        best_alpha = new_alpha
-        return best_score, best_alpha, direction
+        best_param = new_param
+        return best_score, best_param, direction
         
     else:
-        # Try going down (-)
-        new_alpha = best_alpha - precision * multiplier * direction
-        model = algorithm(alpha=new_alpha)
+        # Try going the other way (-)
+        new_param = best_param - precision * multiplier * direction
+        params[param] = new_param
+        model = algorithm(**params)
         new_score = np.mean(cross_val_score(model, X, y, cv=cv))
         if new_score > best_score:
             best_score = new_score
-            best_alpha = new_alpha
-            return best_score, best_alpha, -direction
+            best_param = new_param
+            return best_score, best_param, -direction
         
         else:
-            # Change multiplier
-            new_multiplier = multiplier/change
+            # No progress made, so change multiplier
             return check_alpha_selection(algorithm, X, y, cv, best_score,
-                          best_alpha, new_multiplier, precision, 1)
+                          best_param, multiplier/change, precision, 1)
 
 
-def select_alpha(algorithm, X, y, precision = 0.01, multiplier = 10000, 
-                 alpha = 1, cv=5, verbose=True):
+def select_param(algorithm, X, y, param, params, precision=0.0001, multiplier=1000000,
+                 cv=5, verbose=True, max_iters=1000):
     """
     Basic function intended to iteratively select the best value for alpha
     in those algorithms that use it as a parameter, such as LASSO and
@@ -268,22 +283,27 @@ def select_alpha(algorithm, X, y, precision = 0.01, multiplier = 10000,
     Note: Needs generalization.
     """
     # Model
-    model = algorithm(alpha=alpha)
+    model = algorithm(**params)
     
     # Initial score
     score = np.mean(cross_val_score(model, X, y, cv=cv))
     
-    new_alpha = alpha
+    new_param = params[param]
     new_score = 0
     direction = 1
     improvement = precision + 1
+    iters = 0
     
-    while improvement > precision/100:
+    while improvement > precision:
+        if iters >= max_iters:
+            print("Max iterations reached. Result may be unstable.")
+            break
+
         old_score = new_score
-        new_score, new_alpha, direction = check_alpha_selection(algorithm, 
-                          X, y, cv, score, new_alpha, multiplier, direction)
+        new_score, new_param, direction = check_alpha_selection(algorithm,
+                          X, y, cv, score, new_param, multiplier, direction)
         improvement = new_score - old_score
         if verbose:
-            print("Selected alpha {} which improved performance in: {}".format(new_alpha,
-                                                                              improvement))
-    return new_alpha, new_score
+            print("Selected {}={} which improved performance in: {}".format(param, new_param,
+                                                                            improvement))
+    return new_param, new_score
